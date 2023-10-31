6160370c615343ae0827854d50d28a19c5f38b18
meengguttam <uttu.199@gmail.com>
2023-10-31 21:09:37 +0530

    todolist

---
+++ index.js
@@ -0,0 +1,25 @@
+let myform = document.querySelector('form');
+let inptask = document.getElementById('task');
+let priority = document.getElementById('priority');
+let tbody = document.querySelector('tbody');
+let alldata = [];
+myform.addEventListener('submit',function(abc)
+{
+    let data = {};
+    data.input1 = inptask.value;
+    data.input2 = priority.value;
+    alldata.push(data);
+    tbody.innerHTML = null;
+    alldata.map((element) =>
+    {
+        let row = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 =  document.createElement('td');
+        // console.log(Element.input1);
+        td1.innerText = element.input1;
+        td2.innerText = element.input2;
+        row.append(td1,td2)
+        tbody.append(row);
+    })
+
+})
